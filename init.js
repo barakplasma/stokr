f04839bf3aa2221da3bb17b6d542bbf64dc6799d
Michael Salaverry <michaels@wix.com>
2017-07-18 18:37:44 +0300

    added mock market cap data

--- init.js
+++ init.js
@@ -23,21 +23,24 @@ function stockDataFetcher() {
     "Name": "Wix.com Ltd.",
     "Change": "0.750000",
     "PercentChange": "+1.51%",
-    "LastTradePriceOnly": "76.099998"
+    "LastTradePriceOnly": "76.099998",
+    "MarketCapitalization": "3.4B"
   },
   {
     "Symbol": "MSFT",
     "Name": "Microsoft Corporation",
     "PercentChange": "-2.09%",
     "Change": "-0.850006",
-    "LastTradePriceOnly": "69.620003"
+    "LastTradePriceOnly": "69.620003",
+    "MarketCapitalization": "7.2B"
   },
   {
     "Symbol": "YHOO",
     "Name": "Yahoo! Inc.",
     "Change": "0.279999",
     "PercentChange": "+1.11%",
-    "LastTradePriceOnly": "50.599998"
+    "LastTradePriceOnly": "50.599998",
+    "MarketCapitalization": "4.8B"
   }
   ]`;
 
@@ -73,7 +76,7 @@ function concatenateStockSymbolAndName(stock) {
 }
 
 <!-- Added event listeners to containers after the HTML was rendered (event delegation)-->
-function addClickHandlerstoDOM() {
+function addClickHandlersToDOM() {
   // very general click handler
   document.querySelector('.container').addEventListener('click', dataIDClickHandler);
   // there is a utility function to removeClickHandlersFromDOM()
