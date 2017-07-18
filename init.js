80d6d5794464ba83fbe519105993365355c3670f
Michael Salaverry <michaels@wix.com>
2017-07-18 14:37:52 +0300

    added todo's from slack https://wix-kickstart-2017.slack.com/archives/C5G408NHK/p1500372143986506

---
+++ init.js
@@ -0,0 +1,4 @@
+<!-- todo - Break down the string creation with functions-->
+<!-- todo - Have one render function that initiates the HTML string creation and pushes to the document with `innerHTML`-->
+<!-- todo - Add event listeners to containers after the HTML was rendered (event delegation)-->
+<!-- todo - On events, find the `data-id` of the item and find it’s data based on that id (only when you need to update it or do something with it)-->
