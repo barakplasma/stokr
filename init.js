49c655d217b62a0a7824a66cd4d4090ff2977769
Michael Salaverry <michaels@wix.com>
2017-07-20 18:04:47 +0300

    arranged arrows using relative positioning

--- init.js
+++ init.js
@@ -71,11 +71,14 @@ function createStockRow(stock) {
       ${stockSettings.changePercentToggle ? stock.MarketCapitalization : stock.PercentChange}
      </span>
      <span class="stock-position" aria-label="Row Up and Down Arrows">
-     <svg width="33px" height="25px" viewBox="0 0 33 25" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+     <svg style="position: relative; top: -.3em;" width="33px" height="25px" viewBox="0 0 33 25" 
+     version="1.1" 
+     xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
       <polygon fill="#8F8F8F" id="UpTriangle" transform="translate(16.500000, 11.000000) scale(-1, 1) translate(-16.500000, -11.000000)" points="16.1891892 3 28 19 5 19">
       </polygon>
      </svg>
-     <svg width="33px" height="25px" viewBox="0 0 33 25" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+     <svg style="position: relative; top: .3em; left: -1.2em;" width="33px" height="25px" 
+     viewBox="0 0 33 25" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
       <polygon fill="#8F8F8F" id="DownTriangle" transform="translate(16.500000, 11.000000) scale(1, -1) translate(-16.500000, -11.000000) " points="16.1891892 3 28 19 5 19">
       </polygon>
      </svg>
